aa243bdfac508ecb9ec04e6207c9e049a1317041
Frantsuzan_YS <niajulya@icloud.com>
2021-09-12 19:57:27 +0300

    Initial Commit

---
+++ HW3.2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW3.2
+//
+//  Created by emmisar on 12.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
